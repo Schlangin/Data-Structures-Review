66b607b1338d5e0a8d0dbbf7a6fe52be330303e2
Schlangin <87454975+Schlangin@users.noreply.github.com>
2021-07-15 18:46:35 -0700

    Add files via upload

---
+++ IfStatements.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class IfStatements {
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner (System.in);
+		int test = 6;
+		
+	if(test < 9) {
+		System.out.println("yes");
+		}else {
+			System.out.println("this is else");
+		}
+	}
+}
