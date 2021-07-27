7d745a2640caf173d9353970fe4da62cb251340c
Schlangin <87454975+Schlangin@users.noreply.github.com>
2021-07-26 22:47:53 -0700

    Add files via upload

---
+++ ElseifStatements/apples.java
@@ -0,0 +1,16 @@
+package ElseifStatements;
+
+public class apples {
+	public static void main(String[] args) {
+		int age = 30;
+		
+		if(age >= 60)
+			System.out.println("youre a senior citizen");
+		else if (age >= 50)
+			System.out.println("you are in your 50s");
+		else if (age >= 40)
+			System.out.println("you are in your 40s");
+		else 
+			System.out.println("youre a yung buck");
+	}
+}
