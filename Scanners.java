b3691bf35bcdce22320f8592c8470a5895387535
Mahad Mirza <87454975+Schlangin@users.noreply.github.com>
2023-08-25 12:56:59 -0700

    Delete Scanners.java

--- Scanners.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Scanners {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println(scan.nextLine());
-	}
-}
